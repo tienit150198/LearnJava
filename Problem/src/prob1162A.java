88cff2192d08cfba855aaf0bb6a15a3e100d1269
tienit150198 <tienit150198@gmail.com>
2019-07-04 21:57:15 +0800

    add homework

--- Problem/src/prob1162A.java
+++ Problem/src/prob1162A.java
@@ -1,4 +1,37 @@
+import java.util.Scanner;
 
 public class prob1162A {
+
+	static int n, h, m;
+	
+	static Scanner scn = new Scanner(System.in);
+	static int a[];
+	public static void main(String[] args) {
+		n = scn.nextInt();
+		h = scn.nextInt();
+		m = scn.nextInt();
+		
+		a = new int[n+1];
+		for(int i = 1 ; i <= n ; i++)
+			a[i] = h;
+		
+		int l, r, x;
+		for(int i = 0; i < m ; i++) {
+			l = scn.nextInt();
+			r = scn.nextInt();
+			x = scn.nextInt();
+
+			for(int j = l ; j <= r ; j++) {
+				a[j] = Math.min(a[j], x);
+			}
+		}
+
+		int res = 0;
+
+		for (int i = 1; i <= n; i++)
+			res += (a[i] * a[i]);
+
+		System.out.println(res);
+	}
 
 }
--- Problem/src/prob1167A.java
+++ Problem/src/prob1167A.java
@@ -1,4 +1,30 @@
+import java.util.Scanner;
 
 public class prob1167A {
-
+	static int t, n;
+	static String str;
+	static Scanner scn = new Scanner(System.in);
+	public static void main(String[] args) {
+		t = Integer.parseInt(scn.nextLine());
+		
+		while(t != 0)
+		{
+			n = Integer.parseInt(scn.nextLine());
+			str = scn.nextLine();
+			boolean ok = false;
+			for(int i = 0 ; i < str.length() ; i++) {
+				if(str.charAt(i) == '8' && (n - i) > 10) {
+					ok = true;
+					break;
+				}
+			}
+			
+			if(ok)
+				System.out.println("YES");
+			else
+				System.out.println("NO");
+			
+			t--;
+		}
+	}
 }
